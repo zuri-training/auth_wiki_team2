8f3dbef4d73e34a52466643d6cb9dde1d3fc5742
Olaniyan Daniel Olusegun <danielolaniyan1@gmail.com>
2022-07-28 20:33:36 +0100

    Update urls.py
    Static Urlpatterns included

--- authwiki_project/urls.py
+++ authwiki_project/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+
