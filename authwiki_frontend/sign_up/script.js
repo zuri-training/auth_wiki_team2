d13e5b33b0b5aa18a180c5a71ae720b72df4aab5
Osomhe1 <osomhe.aleogho@gmail.com>
2022-08-01 05:22:34 +0100

    confirm password

--- authwiki_frontend/sign_up/script.js
+++ authwiki_frontend/sign_up/script.js
@@ -14,3 +14,21 @@ function myFunction() {
         z.style.display = 'block'
     }
 }
+
+
+function confirmPassword() {
+    var x = document.getElementById('myInput2');
+    var y = document.getElementById('hide3');
+    var z = document.getElementById('hide4');
+
+    if(x.type === 'password'){
+        x.type = 'text'
+        y.style.display = 'block';
+        z.style.display = 'none'
+    }
+    else{
+        x.type = 'password'
+        y.style.display = 'none';
+        z.style.display = 'block'
+    }
+}
