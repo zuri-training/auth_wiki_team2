fad06d4bff11808c5c8b20fd8ca2a8b3b4a871ea
MAV3R1CK <eddymav247@gmail.com>
2022-08-05 10:12:37 +0100

    google authentication on all pages

--- authwiki_project/urls.py
+++ authwiki_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authwiki_app.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
