b56c657d2054dfd44ea556df8fa99c716a2efd1d
kassy <kausaratg@gmail.com>
2022-08-05 02:17:22 +0100

    library_model

--- authwiki_project/settings.py
+++ authwiki_project/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
+MEDIA_URL = '/media/'
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
